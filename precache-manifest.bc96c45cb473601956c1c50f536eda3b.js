5088ad434a9d8995fa90e64c4038bffdad20fab5
melissa <melissazhu223@gmail.com>
2018-10-25 14:25:05 +1100

    Updates

---
+++ precache-manifest.bc96c45cb473601956c1c50f536eda3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af2707f382313291327d5437174edd87",
+    "url": "/weather/static/media/sky.af2707f3.jpg"
+  },
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "170ab446167231d85b38",
+    "url": "/weather/static/js/main.170ab446.chunk.js"
+  },
+  {
+    "revision": "f887106c0b874be259e3",
+    "url": "/weather/static/js/1.f887106c.chunk.js"
+  },
+  {
+    "revision": "170ab446167231d85b38",
+    "url": "/weather/static/css/main.4efc89ff.chunk.css"
+  },
+  {
+    "revision": "d78405f8b0c4cedebd8be5f0eb9c76d5",
+    "url": "/weather/index.html"
+  }
+];

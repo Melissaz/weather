f8f164ae3fef28ff9316ddf56227476ecfaff2d1
melissa <melissazhu223@gmail.com>
2018-10-26 11:07:04 +1100

    changed footer

--- src/weather/Footer.js
+++ src/weather/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer(){
     return(
         <footer className="weather-channel__footer">
-        <p> Powered by React</p>
+        <p> Designed by Melissa Zhu</p>
       </footer>
     );
 }
